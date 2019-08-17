8ae4868a394a375bee404484e897c58e884b02a8
Christopher Haws <Christopher.Haws@arvato.com>
2019-08-16 23:57:53 -0700

    Add ef core 2.2 migration

---
+++ EFCore3MigrationBug/Migrations/20190817065737_EFCore22Migration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore3MigrationBug.Migrations
+{
+    public partial class EFCore22Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    BlogId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    BlogId = table.Column<Guid>(nullable: false),
+                    PostId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => new { x.BlogId, x.PostId });
+                    table.ForeignKey(
+                        name: "FK_Post_Blog_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blog",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attachment",
+                columns: table => new
+                {
+                    BlogId = table.Column<Guid>(nullable: false),
+                    PostId = table.Column<Guid>(nullable: false),
+                    AttachmentId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachment", x => new { x.BlogId, x.PostId, x.AttachmentId })
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_Attachment_Post_BlogId_PostId",
+                        columns: x => new { x.BlogId, x.PostId },
+                        principalTable: "Post",
+                        principalColumns: new[] { "BlogId", "PostId" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attachment");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}
